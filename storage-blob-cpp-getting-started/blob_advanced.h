be6162b02bfe0e78d615ba247c6e2819914b1ad5
Rajesh Yarlagadda <rayarlag@microsoft.com>
2018-01-05 14:41:17 -0800

    copying blobs across storage accounts

--- storage-blob-cpp-getting-started/blob_advanced.h
+++ storage-blob-cpp-getting-started/blob_advanced.h
@@ -20,20 +20,24 @@ using namespace azure::storage;
 class blob_advanced
 {
 public:
-  blob_advanced();
-  ~blob_advanced();
+	blob_advanced();
+	~blob_advanced();
 
-  static void list_containers(cloud_blob_client blob_client);
-  static void set_cors_rules(cloud_blob_client blob_client);
-  static void lease_blob(cloud_blob_client blob_client);
-  static void lease_container(cloud_blob_client blob_client);
-  static void copy_blob(cloud_blob_client blob_client);
-  static void file_upload_with_blocks(cloud_blob_client blob_client);
-  static void page_blob_operations(cloud_blob_client blob_client);
-  static void set_service_properties(cloud_blob_client blob_client);
-  static void set_metadata_and_properties(cloud_blob_client blob_client);
-  static void set_container_acl(cloud_blob_client blob_client);
+	static void list_containers(cloud_blob_client blob_client);
+	static void set_cors_rules(cloud_blob_client blob_client);
+	static void list_blobs(cloud_blob_client blob_client, utility::string_t container_name);
+	static void lease_container(cloud_blob_client blob_client);
+	void blob_advanced::lease_blob(cloud_blob_client blob_client);
+	static void copy_container(cloud_blob_client blob_client, utility::string_t source, utility::string_t destination);
+	static void copy_account(cloud_blob_client source_blob_client, cloud_blob_client dest_blob_client);
+	static void blob_advanced::copy_container_account(cloud_blob_client source_blob_client, cloud_blob_client dest_blob_client, utility::string_t source_container);
+	static void copy_blob(cloud_blob_client blob_client);
+	static void file_upload_with_blocks(cloud_blob_client blob_client);
+	static void page_blob_operations(cloud_blob_client blob_client);
+	static void set_service_properties(cloud_blob_client blob_client);
+	static void set_metadata_and_properties(cloud_blob_client blob_client);
+	static void set_container_acl(cloud_blob_client blob_client);
 private:
-  static cloud_blob_container create_container(cloud_blob_client blob_client, utility::string_t container_name);
+	static cloud_blob_container create_container(cloud_blob_client blob_client, utility::string_t container_name);
 };
 
--- storage-blob-cpp-getting-started/storage-getting-started.cpp
+++ storage-blob-cpp-getting-started/storage-getting-started.cpp
@@ -20,40 +20,43 @@
 
 using namespace azure::storage;
 
-void run_storage_blob_samples(utility::string_t storage_connection_string);
+void run_storage_blob_samples();
 
 int main()
 {
-  // *************************************************************************************************************************
-  // Instructions: This sample can be run using either the Azure Storage Emulator that installs as part of the Windows Azure SDK (in Windows only) - or by
-  // updating the storage_connection_string with your AccountName and Key. 
-  // 
-  // To run the sample using the Storage Emulator (Windows Azure SDK)
-  //      1. Start the Azure Storage Emulator (once only) by pressing the Start button or the Windows key and searching for it
-  //         by typing "Azure Storage Emulator". Select it from the list of applications to start it.
-  //      2. Set breakpoints and run the project using F10. 
-  // 
-  // To run the sample using the Storage Service
-  //      1. Update the storage_connection_string variable for the emulator (UseDevelopmentStorage=True) and
-  //         replace it with the storage_connection_string for the storage service (AccountName=[]...)
-  //      2. Create a Storage Account through the Azure Portal and provide your [AccountName] and [AccountKey] in 
-  //         the App.Config file. See http://go.microsoft.com/fwlink/?LinkId=325277 for more information
-  //      3. Set breakpoints and run the project using F10. 
-  // 
-  // *************************************************************************************************************************
+	// *************************************************************************************************************************
+	// Instructions: This sample can be run using either the Azure Storage Emulator that installs as part of the Windows Azure SDK (in Windows only) - or by
+	// updating the storage_connection_string with your AccountName and Key. 
+	// 
+	// To run the sample using the Storage Emulator (Windows Azure SDK)
+	//      1. Start the Azure Storage Emulator (once only) by pressing the Start button or the Windows key and searching for it
+	//         by typing "Azure Storage Emulator". Select it from the list of applications to start it.
+	//      2. Set breakpoints and run the project using F10. 
+	// 
+	// To run the sample using the Storage Service
+	//      1. Update the storage_connection_string variable for the emulator (UseDevelopmentStorage=True) and
+	//         replace it with the storage_connection_string for the storage service (AccountName=[]...)
+	//      2. Create a Storage Account through the Azure Portal and provide your [AccountName] and [AccountKey] in 
+	//         the App.Config file. See http://go.microsoft.com/fwlink/?LinkId=325277 for more information
+	//      3. Set breakpoints and run the project using F10. 
+	// 
+	// *************************************************************************************************************************
 
-  const utility::string_t storage_connection_string(U("UseDevelopmentStorage=true"));
+	
 
-  try
-  {
-    run_storage_blob_samples(storage_connection_string);
-  }
-  catch (const azure::storage::storage_exception& e)
-  {
-    ucout << U("Error:") << e.what() << std::endl << U("Unexpected exception while running the sample.") << std::endl;
-  }
+	const utility::string_t source_connection_string(_XPLATSTR("DefaultEndpointsProtocol=https;AccountName=ssqagarda;AccountKey=kLrTPlcKMGC1EdvPP0k1zWeCAedeVhCGhAzTVeceG4QiHdXJZP01lzHGVUGb4N3y7e6M7u+NpiJj8x/USIDSPg=="));
 
-  return 0;
+
+	try
+	{
+		run_storage_blob_samples();
+	}
+	catch (const azure::storage::storage_exception& e)
+	{
+		ucout << U("Error:") << e.what() << std::endl << U("Unexpected exception while running the sample.") << std::endl;
+	}
+
+	return 0;
 }
 
 ///
@@ -61,46 +64,66 @@ int main()
 /// Blob storage stores unstructured data such as text, binary data, documents or media files.
 /// Blobs can be accessed from anywhere in the world via HTTP or HTTPS.
 ///
-void run_storage_blob_samples(utility::string_t storage_connection_string)
+void run_storage_blob_samples()
 {
-  // Parse the connection string
-  cloud_storage_account storage_account = cloud_storage_account::parse(storage_connection_string);
+	const utility::string_t local_connection_string(U("UseDevelopmentStorage=true"));
 
-  cloud_blob_client blob_client = storage_account.create_cloud_blob_client();
+	const utility::string_t source_connection_string(_XPLATSTR("DefaultEndpointsProtocol=https;AccountName=storsimpletestlimitless1;AccountKey=RFx5UQXpPZkQUOd7C0F4mVB1xulmSezO9uhsh6GdBuSchvF/4986Ye43GFfrI9kERvpJjJPbGZehRdOVZPJchw=="));
 
-  // basic operations with block blobs
-  blob_basic::block_blob_operations(blob_client);
+	const utility::string_t dest_connection_string(_XPLATSTR("DefaultEndpointsProtocol=https;AccountName=ssqagarda;AccountKey=kLrTPlcKMGC1EdvPP0k1zWeCAedeVhCGhAzTVeceG4QiHdXJZP01lzHGVUGb4N3y7e6M7u+NpiJj8x/USIDSPg=="));
 
-  //basic operations with append blobs
-  blob_basic::append_blob_operations(blob_client);
+	// Parse the connection string
+	cloud_storage_account source_storage_account = cloud_storage_account::parse(source_connection_string);
 
-  // list containers
-  blob_advanced::list_containers(blob_client);
+	cloud_blob_client source_blob_client = source_storage_account.create_cloud_blob_client();
 
-  // copy blobs
-  blob_advanced::copy_blob(blob_client);
+	cloud_storage_account dest_storage_account = cloud_storage_account::parse(dest_connection_string);
 
-  // file upload with blocks
-  blob_advanced::file_upload_with_blocks(blob_client);
+	cloud_blob_client dest_blob_client = dest_storage_account.create_cloud_blob_client();
 
-  // lease blob for exclusive access
-  blob_advanced::lease_blob(blob_client);
+	cloud_storage_account local_storage_account = cloud_storage_account::parse(local_connection_string);
 
-  // lease container for exclusive access
-  blob_advanced::lease_container(blob_client);
+	cloud_blob_client local_blob_client = local_storage_account.create_cloud_blob_client();
 
-  // page blob operations
-  blob_advanced::page_blob_operations(blob_client);
 
-  // set cors rules for the blob service
-  blob_advanced::set_cors_rules(blob_client);
+	// basic operations with block blobs
+	//blob_basic::block_blob_operations(blob_client);
 
-  // set service properties for the blob service
-  blob_advanced::set_service_properties(blob_client);
+	//basic operations with append blobs
+	// blob_basic::append_blob_operations(blob_client);
 
-  // set container and blob metadata and properties
-  blob_advanced::set_metadata_and_properties(blob_client);
+	// blob_advanced::list_blobs(blob_client, U("test"));
 
-  // set container permissions
-  blob_advanced::set_container_acl(blob_client);
+	// list containers
+	blob_advanced::list_containers(local_blob_client);
+
+	// copy container
+	//blob_advanced::copy_container(blob_client, U("testingaccess1blockblob"), U("final"));
+
+	// copy across accounts
+	blob_advanced::copy_account(local_blob_client, dest_blob_client);
+
+	// file upload with blocks
+	// blob_advanced::file_upload_with_blocks(blob_client);
+
+	// lease blob for exclusive access
+	// blob_advanced::lease_blob(blob_client);
+
+	// lease container for exclusive access
+	// blob_advanced::lease_container(blob_client);
+
+	// page blob operations
+	// blob_advanced::page_blob_operations(blob_client);
+
+	// set cors rules for the blob service
+	// blob_advanced::set_cors_rules(blob_client);
+
+	// set service properties for the blob service
+	//  blob_advanced::set_service_properties(blob_client);
+
+	// set container and blob metadata and properties
+	//  blob_advanced::set_metadata_and_properties(blob_client);
+
+	// set container permissions
+	//  blob_advanced::set_container_acl(blob_client);
 }
